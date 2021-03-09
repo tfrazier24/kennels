f0e05e86c6ebdcf7d3ba462956324c333ab9e582
Thomas Frazier <tfrazier2424@gmail.com>
2021-03-09 13:33:21 -0500

    added ability to search by either upper or lowercase sensitivity

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -24,7 +24,7 @@ export const AnimalList = () => {
     if (searchTerms !== "") {
       // If the search field is not blank, display matching animals
       const subset = animals.filter((animal) =>
-        animal.name.toLowerCase().includes(searchTerms)
+        animal.name.toLowerCase().includes(searchTerms) || animal.name.includes(searchTerms)
       );
       setFiltered(subset);
     } else {
